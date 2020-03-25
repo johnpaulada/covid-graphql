984d700b1c1368fedee44d9d66bf5da0c1adf45a
John Paul Ada <johnpaulada@gmail.com>
2020-03-25 21:32:56 +0800

    feat: add filter by country name

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ A GraphQL wrapper for the https://corona-stats.online/ site.
 
 ## Stuff to add
 - [ ] Other fields
-- [ ] Filtering
 - [ ] More Stats
--- src/resolvers.js
+++ src/resolvers.js
@@ -2,10 +2,33 @@ require('isomorphic-fetch')
 
 const CORONA_STATS_API = 'https://corona-stats.online/?format=json'
 
+const filterHandlers = {
+  country: country => data => {
+    return data.filter(item => item.country === country)
+  }
+}
+
+const filterWith = filters => data => {
+  return Object.entries(filterHandlers).reduce((filteredData, [name, handler]) => {
+    return name in filters
+      ? handler(filters[name])(filteredData)
+      : filteredData
+  }, data) 
+}
+
 const resolvers = {
     Query: {
-      countryStats: async () => (await (await fetch(CORONA_STATS_API)).json()).data
-    },
+      countryStats: async (_, args) => {
+        const response = await fetch(CORONA_STATS_API)
+        const json = await response.json()
+        const data = json.data
+        const { filter } = args
+
+        return filter
+          ? filterWith(filter)(data)
+          : data
+      }
+    }
   };
   
 export default resolvers;
