fd23b8c86afa77d07317dc0f4ca70836c6a93a7f
John Paul Ada <johnpaulada@gmail.com>
2020-03-25 19:42:40 +0800

    fix: added call to data to resolved response json object

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,7 +4,7 @@ const CORONA_STATS_API = 'https://corona-stats.online/?format=json'
 
 const resolvers = {
     Query: {
-      countryStats: async () => await (await fetch(CORONA_STATS_API)).json()
+      countryStats: async () => (await (await fetch(CORONA_STATS_API)).json()).data
     },
   };
   
